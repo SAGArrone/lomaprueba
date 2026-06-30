469aaf61676e66cd32689f1b2fa00d8482344ab6
SAGArrone <39418371+SAGArrone@users.noreply.github.com>
2026-06-30 15:26:25 -0300

    Delete l10n_ar_rg5329_perception_v5 directory

--- l10n_ar_rg5329_perception_v5/models/product_template.py
+++
@@ -1,9 +0,0 @@
-from odoo import models
-
-
-class ProductTemplate(models.Model):
-    _inherit = "product.template"
-
-    # RG 5329 no se asigna en productos.
-    # Se calcula dinamicamente en facturas por cliente, documento, categoria y acumulado por alicuota.
-    pass
