7b22260334f50a6b37c4f812ce40cbb9d9aee634
SAGArrone <39418371+SAGArrone@users.noreply.github.com>
2026-07-01 08:48:45 -0300

    Add files via upload

--- l10n_ar_afipws_odoo19/__init__.py
+++ l10n_ar_afipws_odoo19/__init__.py
@@ -1,6 +1,2 @@
-##############################################################################
-# For copyright and license notices, see __manifest__.py file in module root
-# directory
-##############################################################################
-from . import wizard
 from . import models
+from . import wizard
